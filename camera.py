26efed4855b6962580a2b4e9561c6fc8852df192
ysak-y <yoshiaki.0614@gmail.com>
2017-03-08 23:33:45 +0900

    Upload to s3

--- camera.py
+++ camera.py
@@ -1,7 +1,11 @@
 from picamera import PiCamera
 from time import sleep
+import boto3
 
 camera = PiCamera()
 camera.start_preview()
 sleep(2)
-camera.capture('foo.jpg')
+camera.capture('picture.jpg')
+
+s3 = boto3.client('s3')
+s3.upload_file('picture.jpg', 'yoshiaki-raspi-camera', 'picture.jpg')
